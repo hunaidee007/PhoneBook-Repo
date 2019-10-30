4c45b8c6573f6dc069ac8f413cc7184c7df3dfca
Hunaid Husain <HunaidIqbal.Husain@santam.co.za>
2019-10-30 14:51:29 +0200

    Added Demo GIF and changed the script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,10 @@
-insert into phonebook values (1, 'Phonebook');
-insert into phonebook values (2, 'Hunaid Phonebook');
-insert into phonebook values (3, 'Akbnar Phonebook');
-insert into phonebook values (4, 'Birbalk Phonebook');
-insert into phonebook values (5, 'aaa Phonebook');
+insert into phonebook values (1, 'Home');
+insert into phonebook values (2, 'Office');
 
-insert into contact values(1,'hunaid',1);
-insert into contact values(2,'Husian',1);
-insert into contact values(3,'Iqbal',1);
-insert into contact values(4,'Shehnaz',2);
-insert into contact values(5,'Shehnaz 1',2);
-insert into contact values(6,'Shehnaz 2',2);
+
+insert into contact values(1,'Dries',1);
+insert into contact values(2,'Dian',1);
+insert into contact values(3,'Chilton',1);
+insert into contact values(4,'Charles',1);
+insert into contact values(5,'Clayton',1);
+insert into contact values(6,'Danyal',1);
